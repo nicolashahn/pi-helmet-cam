89178a03f63bc181775fb5add1618d930073cd68
nicolashahn <nicolas@stonespring.org>
2017-03-07 21:53:17 +0000

    continuous recording with split videos

--- camera.py
+++ camera.py
@@ -1,6 +1,8 @@
 from picamera import PiCamera
 from time import sleep
 from datetime import datetime
+from os import mkdir
+from os.path import isdir
 
 
 # Constants
@@ -8,21 +10,39 @@ from datetime import datetime
 # to specify lines not to run during actual use
 testing = True
 
+videodir = 'video'
 logfile = 'videolog.log'
+filetype = 'h264'
 
 # (pixel width, height)
 resolution = (1400,1050)
 
-# second to film each video
-interval = 10
+# number of seconds to film each video
+interval = 5
 
 
-def record(camera, filename, interval):
-	""" record a single video of given length to specified file """
+def generate_filename(videodir, timestamp, counter, filetype):
+	filename_prefix = '{}/{}'.format(videodir, timestamp)
+	if not isdir(filename_prefix):
+		if testing: print 'Creating directory {}'.format(filename_prefix)
+		mkdir(filename_prefix)
+	filename =  "{}/{}-{}.{}".format(filename_prefix, timestamp, counter, filetype)
 	if testing: print 'Recording {}'.format(filename)
+	return filename
+
+
+def continuous_record(camera, videodir, timestamp, filetype, interval):
+	""" record <interval> second files with prefix """
+	counter = 0
 	if testing: camera.start_preview()
-	camera.start_recording(filename)
-	sleep(interval)
+	initial_filename = generate_filename(videodir, timestamp, counter, filetype)
+	camera.start_recording(initial_filename)
+	camera.wait_recording(interval)
+	while(True):
+		counter += 1
+		split_filename = generate_filename(videodir, timestamp, counter, filetype)
+		camera.split_recording(split_filename)
+		camera.wait_recording(interval)
 	camera.stop_recording()
 	if testing: camera.stop_preview()
 	
@@ -30,14 +50,12 @@ def record(camera, filename, interval):
 def main():
 	with PiCamera() as camera:
 
-
 		# Initialization
-
 		camera.resolution = resolution
 		timestamp = str(datetime.utcnow()).replace(' ','-').replace(':','-')
-		filename = 'video/{}.h264'.format(timestamp)
 
-		record(camera, filename, interval)
+		# start recording, chunking files every <interval> seconds
+		continuous_record(camera, videodir, timestamp, filetype, interval)
 
 
 if __name__ == "__main__":
