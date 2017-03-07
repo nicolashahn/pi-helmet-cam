95b7ea006bd66994442f8ebae27631b8ae98556e
nicolashahn <nicolas@stonespring.org>
2017-03-07 20:45:00 +0000

    small organization refactor

--- camera.py
+++ camera.py
@@ -5,26 +5,40 @@ from datetime import datetime
 
 # Constants
 
+# to specify lines not to run during actual use
 testing = True
+
+logfile = 'videolog.log'
+
+# (pixel width, height)
 resolution = (1400,1050)
 
+# second to film each video
+interval = 10
 
 
-with PiCamera() as camera:
-
-
-	# Initialization
-
-	camera.resolution = resolution
-	timestamp = str(datetime.utcnow()).replace(' ','-').replace(':','-')
-	filename = '/home/pi/cam/video/{}.h264'.format(timestamp)
-
-
-	# Recording
-
-	print 'Recording {}'.format(filename)
+def record(camera, filename, interval):
+	""" record a single video of given length to specified file """
+	if testing: print 'Recording {}'.format(filename)
 	if testing: camera.start_preview()
 	camera.start_recording(filename)
-	sleep(10)
+	sleep(interval)
 	camera.stop_recording()
 	if testing: camera.stop_preview()
+	
+
+def main():
+	with PiCamera() as camera:
+
+
+		# Initialization
+
+		camera.resolution = resolution
+		timestamp = str(datetime.utcnow()).replace(' ','-').replace(':','-')
+		filename = 'video/{}.h264'.format(timestamp)
+
+		record(camera, filename, interval)
+
+
+if __name__ == "__main__":
+	main()
