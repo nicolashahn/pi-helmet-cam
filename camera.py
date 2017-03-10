0424aedd1e59849b863f59f408af4fea6fa7f4a7
Nicolas Hahn <nicolas@stonespring.org>
2017-03-10 11:33:25 -0800

    make_room() added but not yet used

--- camera.py
+++ camera.py
@@ -3,6 +3,8 @@ from time import sleep
 from datetime import datetime
 from os import mkdir
 from os.path import isdir
+from os import listdir
+from shutil import rmtree
 import sys
 import subprocess
 
@@ -12,7 +14,6 @@ import subprocess
 testing = False
 
 videodir = 'video'
-logfile = 'videolog.log' #TODO
 filetype = 'h264'
 
 # how many 0s to put in front of counter number
@@ -28,6 +29,18 @@ interval = 5
 
 # what % of disk space must be free to start a new video
 required_free_space_percent = 10
+
+
+def make_room():
+	""" clear oldest video """
+	sorted_videos = sorted(listdir(video))
+	if sorted_videos:
+		oldest_video = sorted_videos[0]
+		if testing: print 'Removing oldest video: {}'.format(oldest_video)
+		shutil.rmtree(oldest_video) # may not have permission if running as pi and video was created by root
+	else:
+		if testing: print 'No videos in directory {}, cannot make room'.format(videodir)
+
 
 def enough_disk_space():
 	""" return true if we have enough space to start a new video """
@@ -48,7 +61,7 @@ def generate_filename(videodir, timestamp, counter, filetype):
 		if testing: print 'Creating directory {}'.format(filename_prefix)
 		mkdir(filename_prefix)
 	zfill_counter = str(counter).zfill(zfill_decimal)
-	filename =  "{}/{}-{}.{}".format(filename_prefix, timestamp, zfill_counter, filetype)
+	filename =  '{}/{}-{}.{}'.format(filename_prefix, timestamp, zfill_counter, filetype)
 	if testing: print 'Recording {}'.format(filename)
 	return filename
 
@@ -76,7 +89,9 @@ def main():
 		camera.resolution = resolution
 		camera.framerate = framerate
 		timestamp = str(datetime.now()).replace(' ','-').replace(':','-')
-		enough_disk_space()
+		while not enough_disk_space():
+			# make_room()
+			pass
 
 		# start recording, chunking files every <interval> seconds
 		continuous_record(camera, videodir, timestamp, filetype, interval)
